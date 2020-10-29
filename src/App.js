f10beeeab9ef9a4145751dc40228c32e64fc4a66
PlayingOnGitHub <playingongithub@gmail.com>
2020-10-29 19:52:21 -0400

    Initial Commit!

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './components/List';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>
+                Initial Commit!
+            </div>
+        )
+    }
+}
+
+export default App;
